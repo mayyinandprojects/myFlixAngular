235fb3ed4be314ebe7f7c0cc49184dfcf5978c1a
may_ <lammayyin@gmail.com>
2024-10-13 23:12:49 +0200

    create component user-registration-form component

--- myFlix-Angular-client/src/app/app-routing.module.ts
+++ myFlix-Angular-client/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
+// import { LoginComponent } from './login/login.component';
+// import { MovieListComponent } from './movie-list/movie-list.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'movies', component: MovieListComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  // { path: 'login', component: LoginComponent },
+  // { path: 'movies', component: MovieListComponent },
+  // { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
--- myFlix-Angular-client/src/app/app.module.ts
+++ myFlix-Angular-client/src/app/app.module.ts
@@ -3,21 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { LoginComponent } from './login/login.component';
+// import { MovieListComponent } from './movie-list/movie-list.component';
+// import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent,
-    LoginComponent
+    UserRegistrationFormComponent,
+    UserLoginFormComponent,
+    // MovieListComponent,
+    // LoginComponent
   ],
-  imports: [
-    FormsModule,
+  imports: [ 
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
