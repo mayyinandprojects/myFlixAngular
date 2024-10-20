02f1e5f1bc9d8931b7dac50448f4df2ea0793579
may_ <lammayyin@gmail.com>
2024-10-21 01:10:36 +0200

    update routing for movie-details and profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard'; 
 // import { LoginComponent } from './login/login.component';
 // import { MovieListComponent } from './movie-list/movie-list.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 const routes: Routes = [
-  // { path: 'login', component: LoginComponent },
-  // { path: 'movies', component: MovieListComponent },
-  // { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'movie-details/:title', component: MovieDetailsComponent },  // Protected route
+  { path: 'profile/:userId', component: ProfileViewComponent, canActivate: [AuthGuard] },   // Protected route
+  { path: 'welcome', component: WelcomePageComponent },                         // Public route
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' }  // Default route
 ];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
