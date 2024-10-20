3be3ba2d20751c5f5ba51b20cc8369a0ef24e507
may_ <lammayyin@gmail.com>
2024-10-21 01:11:11 +0200

    create user.model.ts

---
+++ src/app/user.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+    _id: string;
+    name: string;
+    username: string;
+    password: string;
+    email: string;
+    birthday: string;
+    favorite_movies: string[];
+    __v: number;
+  }
+  
