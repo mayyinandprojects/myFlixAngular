8e63a72cbb4f42a3a40fb76f98f5784cf20cb503
may_ <lammayyin@gmail.com>
2024-10-21 01:12:06 +0200

    update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 // src/app/app.component.ts
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';  // Import your authentication service
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myFlix-Angular-client';
+  title="myFlix Angular"
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  // Check if user is logged in by calling AuthService
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  // Navigate to movies
+  goToMovies(): void {
+    this.router.navigate(['/movies']);
+  }
+
+  // Navigate to user profile
+  goToProfile(): void {
+    const userId = localStorage.getItem('userId');
+    this.router.navigate([`/profile/${userId}`]);
+  }
+
+  // Logout logic
+  logout(): void {
+    this.authService.logout(); // Use the AuthService to logout
+    this.router.navigate(['/welcome']);
+  }
 }
