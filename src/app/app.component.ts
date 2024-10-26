2b3da70a19e6156c3c3d63e77ccd07fc8ae5cb7d
may_ <lammayyin@gmail.com>
2024-10-26 18:20:47 +0200

    add comment with relative path name in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// src\app\app.component.ts
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service'; // Adjust the import path as necessary
